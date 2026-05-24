387226a3a256c51a0966071139cbee6772e9076b
fatintsani <fatin.tsani231@gmail.com>
2026-05-24 23:26:29 +0700

    first commit

--- js/audio-system.js
+++ js/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Sampai Jadi Debu',
-    artist:    'Banda Neira',
+    title:     'Semua Aku Dirayakan',
+    artist:    'Nadin Amizah',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 0,       // seconds — where playback begins
